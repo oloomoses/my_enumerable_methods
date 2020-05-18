3ecc412476d39a436267323d753cc9779334bd0e
oloomoses <olooine@gmail.com>
2020-05-18 23:44:11 +0300

    added my_each enumerable method

--- enumerable.rb
+++ enumerable.rb
@@ -1,5 +1,18 @@
 module Enumerable
-  def my_each
-    # code 
+
+  def my_each(array)
+    for element in array do
+      yield element
+    end
   end
+  
+  
+
+end
+
+include Enumerable
+array = %w[this is a beautiful array of strings]
+
+Enumerable.my_each_with_index(array) do |i, e|
+  p "#{i}: #{e}"
 end
