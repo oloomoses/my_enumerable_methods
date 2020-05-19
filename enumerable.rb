63b639196187519552f21f4a84e1f0cb1f1cdf49
oloomoses <olooine@gmail.com>
2020-05-20 01:41:39 +0300

    created the my_each_with_index enumerable method

--- enumerable.rb
+++ enumerable.rb
@@ -5,18 +5,30 @@ module Enumerable
       yield element
     end
   end
-  
+
   def my_each_with_index(array)
     for element in array do
-      yield array.index(element), element
+      yield element, array.index(element)
     end
   end
+  
+
+  def my_all()?
+  end
+
+  def my
 
 end
 
 include Enumerable
 array = %w[this is a beautiful array of strings]
 
-Enumerable.my_each_with_index(array) do |i, e|
-  p "#{i}: #{e}"
+
+
+# Enumerable.my_each(array) do |k|
+#   puts k
+# end
+
+Enumerable.my_select(array) do |val|
+  val != "beautiful"
 end
